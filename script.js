2f9e48266814654fe66eb163086715e615cd2997
Thad Rigby <thadrigby@gmail.com>
2014-04-21 20:51:58 -0600

    Posting newsfeed

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+	$.ajax('https://api.parse.com/1/classes/NewsItem', 
+	  {
+	    beforeSend: function(request) {
+	      request.setRequestHeader('X-Parse-Application-Id', 'wp4sBsBXBMygewTekXeVDgfHALM1mM1tJ4FqS1mK');
+	      request.setRequestHeader('X-Parse-REST-API-Key', 'mHijvpVtVXd3rL9i7b863e6EqUMdAKvdveOBsCWz');
+	        }
+	    }
+).done(function(response) {
+	//console.log(response.results);
+	// var stuffToPostOnOurSite = response.results;
+	// console.log(stuffToPostOnOurSite);
+  	// $(".newsfeed").text(response.results);
+  	// for (var i = 0; i<response.results.length; i++){
+  	// 	console.log(response.results[i])
+  	// 	$(".newsfeed").append(response.results[i].title + "<br><br><br>" + response.results[i].body + "<br><br><br>");
+  	// }
+  	
 
+});
 });
